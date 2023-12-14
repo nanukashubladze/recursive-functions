b4b821ad8b524de06059dd4dc66650195596a551
nanukashubladze <123475291+nanukashubladze@users.noreply.github.com>
2023-12-15 00:54:45 +0400

    done

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-function product(x, y) {
+// function product(x, y) {
 
-  if (x === 0 || y === 0) {
-      return 0;
-  }
-  return x + product(x, y - 1);
-}
-
-
-
-console.log(product(2, 3));
+//   if (x === 0 || y === 0) {
+//       return 0;
+//   }
+//   return x + product(x, y - 1);
+// }
 
 
 
-function reverseString(str) {
-  if (str.length <= 1) {
-      return str;
-  } else {
-      return str.slice(-1) + reverseString(str.slice(0, -1));
-  }
-}
+// console.log(product(2, 3));
 
-console.log(reverseString("hello"));
+
+
+// function reverseString(str) {
+//   if (str.length <= 1) {
+//       return str;
+//   } else {
+//       return str.slice(-1) + reverseString(str.slice(0, -1));
+//   }
+// }
+
+// console.log(reverseString("hello"));
 
 
 
@@ -29,7 +29,11 @@ function countDownClockSimulation(seconds) {
   if (seconds <= 0) {
     document.getElementById('countdown').innerHTML = 'Countdown expired!';
   } else {
-    document.getElementById('countdown').innerHTML = `Time remaining: 24 hours:0 minutes: ${seconds} seconds`;
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    document.getElementById('countdown').innerHTML = `Time remaining: ${hours} hours: ${minutes} minutes: ${remainingSeconds} seconds`;
 
     setTimeout(function () {
       countDownClockSimulation(seconds - 1);
@@ -37,4 +41,19 @@ function countDownClockSimulation(seconds) {
   }
 }
 
-countDownClockSimulation(60);
+countDownClockSimulation(3665);
+
+function convertSecondsToHoursMinutesAndSeconds(seconds) {
+  if (seconds < 0) {
+    console.log("It's a negative number");
+    return;
+  }
+
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  console.log(`${hours} hours, ${minutes} minutes, and ${remainingSeconds} seconds.`);
+}
+
+convertSecondsToHoursMinutesAndSeconds(3665);
